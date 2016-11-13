57d71b8692f59c2d8605e8ed58ede5f26f030e48
Riza <Riza@DESKTOP-IEBN81F>
2016-11-13 20:53:21 +0700

    12141422 deklarasi variabel

---
+++ src/app12141422.java
@@ -0,0 +1,11 @@
+public class app12141422 {
+    String nama;
+    byte level = 1;
+    byte kesempatan = 5;
+    short tebak;
+    short banyak=0;
+    int random, score=0;
+    char jawab;
+    
+    
+}
